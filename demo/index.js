b788678527d255ff7c32fef252d9583bf81d5d71
jjcapellan <soycape@hotmail.com>
2024-01-23 22:08:05 +0100

    packages update

--- demo/index.js
+++ demo/index.js
@@ -1,22 +1,24 @@
 import Phaser from 'phaser';
 import Load from './scenes/load.js';
-import Test from './scenes/test.js';
+import Demo from './scenes/demo.js';
 
 // Colors
 window.xbackColor = 0xe9efec;
 window.backColor = '#e9efec';
 window.xfrontColor = 0x555568;
 window.frontColor = '#555568';
+window.xredColor = 0xdd1111;
+window.redColor = '#dd1111';
 
 function runGame() {
     var config = {
         type: Phaser.AUTO,
-        width: 800,
-        height: 600,
+        width: 840,
+        height: 640,
         parent: 'demo',
         backgroundColor: xbackColor,
         roundPixels: true,
-        scene: [Load, Test]
+        scene: [Load, Demo]
     };
 
     new Phaser.Game(config);
